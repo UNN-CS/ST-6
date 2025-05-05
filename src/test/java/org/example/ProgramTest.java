7afea50f9335e90d3c8bc4d4dde528daf0eb1195
WladislawFilatew <fil265401585@mail.ru>
2025-05-05 15:31:10 +0300

    task 1

---
+++ src/test/java/org/example/ProgramTest.java
@@ -0,0 +1,12 @@
+package org.example;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProgramTest {
+
+    @Test
+    public void test_1() {
+        assertEquals(1,1);
+    }
+}
